f40ae60fdbb2423bedfd3780c3477c9217aba9db
Ricky Chuang <songsongyyohya@gmail.com>
2022-03-03 23:45:13 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Google-Blogger-iOS-App-main",
+    name: "Google-Blogger-iOS-App",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Google-Blogger-iOS-App-main",
-            targets: ["Google-Blogger-iOS-App-main"]),
+            name: "Google-Blogger-iOS-App",
+            targets: ["Google-Blogger-iOS-App"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Google-Blogger-iOS-App-main",
+            name: "Google-Blogger-iOS-App",
             dependencies: []),
         .testTarget(
-            name: "Google-Blogger-iOS-App-mainTests",
-            dependencies: ["Google-Blogger-iOS-App-main"]),
+            name: "Google-Blogger-iOS-AppTests",
+            dependencies: ["Google-Blogger-iOS-App"]),
     ]
 )
