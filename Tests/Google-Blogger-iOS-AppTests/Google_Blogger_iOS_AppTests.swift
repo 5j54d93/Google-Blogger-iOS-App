ff127d9c2286692ea9ef119ee0cbfc66295373d6
Ricky Chuang <songsongyyohya@gmail.com>
2022-03-03 23:43:44 +0800

    Add files via upload

---
+++ Tests/Google-Blogger-iOS-AppTests/Google_Blogger_iOS_AppTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Google_Blogger_iOS_App_main
+
+final class Google_Blogger_iOS_App_mainTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Google_Blogger_iOS_App_main().text, "Hello, World!")
+    }
+}
